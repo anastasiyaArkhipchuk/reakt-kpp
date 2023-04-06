c7c6f4e001f3b6258fdabf0c9f0f65bc11712ae6
anastasiyaArkhipchuk <nastyashvec42@gmail.com>
2023-04-06 19:44:04 +0300

    Lesson 2

---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Rating = () => {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+const Star = () => {
+    return (
+        <div>star</div>
+    )
+}
